6f83176fae8f288c819c930bfe4bfe388c048f98
bluewings <mygenie3@naver.com>
2016-03-11 14:28:23 +0900

    Update config.js
    whitelist 방식으로 노출 태그를 제어할 수 있음

--- app/config.js
+++ app/config.js
@@ -6,7 +6,7 @@ angular.module('config', [])
   timezone: 'Asia/Seoul',
   swaggerJson: [
     'http://10.113.217.209:8080/v2/api-docs?group=ncc',
-	'http://10.113.217.209:8080/v2/api-docs?group=tool',
+    'http://10.113.217.209:8080/v2/api-docs?group=tool',
     'https://test-atower.searchad.naver.com/v2/api-docs',
     'http://10.99.196.208:17000/v2/api-docs',
     'assets/json/master-report.json'
@@ -18,6 +18,7 @@ angular.module('config', [])
   menus: ['guides', 'reference', 'samples'],
   tags: {
     pascalCase: true,
+    include: ['~[a-zA-Z]+~'],
     exclude: ['accesscontrol', 'advancedreportcontroller', 'apiapikey', 'apicustomerauth', 'apihistory', 'apikeys', 'apilicenses', 'apimyclientscontroller', 'auth', 'columncontroller', 'customerauth', 'customerlinks', 'history', 'managercandidates', 'nccmanagedkeyword', 'l7~', 'basicerror~', '~endpoint', 'endpoint~', '~controller']
   },
   definitions: {
